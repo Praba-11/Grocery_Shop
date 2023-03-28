484aaff910a8e304fa7630700c275a93890dea3d
Praba-11 <sukumarprabakaran@gmail.com>
2023-03-28 19:07:15 +0530

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,8 @@ public class Main {
             System.out.print("> ");
             Scanner scanner = new Scanner(System.in);
             String command = scanner.nextLine();
-            String cmd[] = command.split("[, ]");
+            String cmnd[] = command.split(",");
+            String cmd[] = command.split("\\s+");
             System.out.println(Arrays.toString(cmd));
             if (cmd[0].equals("end"))
                 break;
@@ -18,10 +19,12 @@ public class Main {
                 if (cmd.length == 3 && cmd[2].equals("help"))
                     exec("Create Unit using following template.\nName, Code, Description, isDividable\nname - text, mandatory with 3 to 30 chars\ncode - text, maximum 4 char, mandatory\ndescription - text\nisdividable - boolean, mandatory");
                 else if (cmd.length == 5) {
-                    String name = cmd[3];
-                    String code = cmd[4];
-                    String desc = cmd[5];
-                    boolean isDividable = Boolean.parseBoolean(cmd[6]);
+                    String command1[] = cmnd[0].split(" ");
+                    String name = cmnd[2];
+                    String code = cmd[3];
+                    String desc = cmd[4];
+                    boolean isDividable = Boolean.parseBoolean(cmd[5]);
+
                 }
             }
         }
